4d98de80eae83170a6beeabb92ec06e96c11727c
berkeguncan <zekiberkeguncan14@gmail.com>
2017-08-18 22:16:34 +0300

    Update remotefunctions.py

--- remotefunctions.py
+++ remotefunctions.py
@@ -1,36 +1,79 @@
-import kivy
-kivy.require("1.10.0")
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
-from kivy.app import App
-from kivy.core.window import Window
-from kivy.uix.modalview import ModalView
-from kumanda import kumanda
-from kivy.uix.screenmanager import ScreenManager,Screen,FadeTransition
 import time
-import sys
-
-class silisim(Screen):
-    pass   
-class Taban(Screen):
-    pass
-class Ekranci(ScreenManager):
-    pass
-
-sunum = Builder.load_file("mainkvm.kv")
-
-class Uygulamam(App):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.kumanda = kumanda()
-    def build(self):
-        return sunum 
-    def sil(self):
-        kanal_adi = silisim.root.ids["delete"].text
-        self.kumanda.kanal_sil(kanal_adi)
-        print("Silindi")
-if __name__ == "__main__":
-    Window.clearcolor = (0,1,1,0)
-    Window.size = 300,700
-    Uygulamam().run()
+import sys    
+class kumanda():
+    def __init__(self,tv_durumu = "Kapalı",tv_ses = 0,anlik_kanal = "TRT",kanal_listesi = ["TRT","Kanal D","Atv"]):
+        self.tv_durumu = tv_durumu
+        self.tv_ses = tv_ses
+        self.anlik_kanal = anlik_kanal
+        self.kanal_listesi = kanal_listesi
+    def tv_ackapa(self):
+        if self.tv_durumu == "Kapalı":
+            print("Televizyon Açılıyor")
+            time.sleep(1)
+            self.tv_durumu = "Açık"
+            print("Televizyon Açıldı")
+            return True
+        elif self.tv_durumu == "Açık":
+            print("Televizyon Kapatılıyor...")
+            time.sleep(1)
+            self.tv_durumu = "Kapalı"
+            print("Televizyon Kapandı.")
+            time.sleep(2)
+            sys.exit()
+            return False
+    def ses_arttir(self):
+        if self.tv_durumu == "Açık":
+            if self.tv_ses < 32:
+                self.tv_ses += 2
+                print(self.tv_ses)
+            elif self.tv_ses == 32:
+                print("Ses Maksimum Seviyede")
+        else:
+            print("Televizyon Kapalı")
+    def ses_azalt(self):
+        if self.tv_durumu == "Açık":
+            if self.tv_ses > 0:
+                self.tv_ses -= 2
+                print(self.tv_ses)
+            elif self.tv_ses == 0:
+                print("Ses Minimum Seviyede")
+        else:
+            print("Televizyon Kapalı")
+    def kanal_ilerlet(self):
+        if self.tv_durumu == "Açık": 
+            try:
+                index = self.kanal_listesi.index(self.anlik_kanal)
+                self.anlik_kanal = self.kanal_listesi[index+1]
+                print("Şu anki kanal \n>>> {}".format(self.anlik_kanal))
+            except(IndexError):
+                self.anlik_kanal = self.kanal_listesi[0]
+        else:
+            print("Televizyon Kapalı")
+    def kanal_gerilet(self):
+        if self.tv_durumu == "Açık":
+            try:
+                index = self.kanal_listesi.index(self.anlik_kanal)
+                self.anlik_kanal = self.kanal_listesi[index-1]
+                print("Şu anki kanal \n>>> {}".format(self.anlik_kanal))
+            except(IndexError):
+                self.anlik_kanal = self.kanal_listesi[0]
+        else:
+            print("Televizyon Kapalı")
+    def kanal_listele(self):
+        if self.tv_durumu == "Açık":
+            print(100*"-")
+            for x in self.kanal_listesi:
+                print(">>>" + x)
+            print(100*"-")
+        else:
+            print("Televizyon Kapalı")
+    def kanal_ekle(self):
+        kanal_adi = input("Kanal Adını Giriniz :")
+        print("Kanal Ekleniyor...")
+        time.sleep(1)
+        self.kanal_listesi.append(">>>"+ kanal_adi)
+        print("Kanal Eklendi.")
+    def kanal_sil(self,kanal_adi):
+        self.kanal_listesi.remove(kanal_adi)
+    def suanki_kanal(self):
+        print("Şu anki Kanal :\n{}".format(self.anlik_kanal))
